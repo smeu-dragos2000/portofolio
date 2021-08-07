81903cf79db4274091fdf20913244cfa057a2e6c
smeu-dragos2000 <56107332+smeu-dragos2000@users.noreply.github.com>
2021-08-07 16:41:18 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,39 +1,88 @@
+// --- Hamburger Menu ---
+
+const hamburger = document.querySelector('.hamburger');
+const Menu = document.querySelector('nav ul');
+let menuItem = document.querySelectorAll('nav ul li');
+
+const showMenu = () => {
+    Menu.classList.toggle('open');
+}
+
+
+
+hamburger.addEventListener('click', showMenu);
+menuItem.forEach(box => box.addEventListener('click', showMenu));
+
+
+// --- Mug-shot transition ---
+
+const animationTime = 300;
+
 $(document).ready(() => {
 
-    $('#me-left').css('opacity', '0');
-    $('#me-right').css('opacity', '0');
+    $('#left-pose').css('opacity', '0');
+    $('#right-pose').css('opacity', '0');
 
     //    Left Side Animation
     $('#artist').on('mouseover', () => {
-        $('#me-left').animate({opacity: '1'}, 400);
-        $('#me-front').animate({opacity: '0'}, 400);
+        $('#left-pose').animate({opacity: '1'}, animationTime);
+        $('#front-pose').animate({opacity: '0'}, animationTime);
     });
 
     $('#artist').on('mouseleave', () => {
-        $('#me-left').animate({opacity: '0'}, 400);
-        $('#me-front').animate({opacity: '1'}, 400);
+        $('#left-pose').animate({opacity: '0'}, animationTime);
+        $('#front-pose').animate({opacity: '1'}, animationTime);
     });
 
     //    Right Side Animation
     $('#coder').on('mouseover', () => {
-        $('#me-right').animate({opacity: '1'}, 400);
-        $('#me-front').animate({opacity: '0'}, 400);
+        $('#right-pose').animate({opacity: '1'}, animationTime);
+        $('#front-pose').animate({opacity: '0'}, animationTime);
     });
 
     $('#coder').on('mouseleave', () => {
-        $('#me-right').animate({opacity: '0'}, 400);
-        $('#me-front').animate({opacity: '1'}, 400);
+        $('#right-pose').animate({opacity: '0'}, animationTime);
+        $('#front-pose').animate({opacity: '1'}, animationTime);
     });
 });
 
+// --- Skills populate ---
+
+let skillsContainer = document.querySelector("#skills");
+
+const skillsRequest = fetch('skills.json')
+    .then (response => response.json())
+    .then (data => {
+        data.forEach(element => {
+            let skillFill = `<div class="skill-fill" style="width: ${element.value}%;"><span>${element.skill}</span></div>`;
+            let skillEmpty = `<div class="skill-empty"></div>`;
+            let skillBar = `<div class="skill-bar">${skillEmpty}${skillFill}</div>`
+            skillsContainer.innerHTML += skillBar;
+
+            // console.log(skillBar);
+        })
+    });
+
+// --- Work Gallery populate ---
+
+let gridContainer = document.querySelector("#grid-container");
+
+const workRequest = fetch('work.json')
+    .then (response => response.json())
+    .then (data => {
+        data.forEach(element => {
+            let itemTitle = `<h3 class="item-title">${element.title}</h3>`;
+            let itemImage = `<img src="${element.img}">`;
+            let itemDescription = `<p>${element.description}</p>`
+            let itemLink = element.link;
+            let item = `<div class="grid-item"><a href="${itemLink}" target="blank">${itemTitle} ${itemImage} ${itemDescription}</a>`
+
+            gridContainer.innerHTML += item;
+        })
+        
+    });
 
 
-//  --- Google Maps API ---
 
-function myMap() {
-    var mapProp= {
-      center:new google.maps.LatLng(51.508742,-0.120850),
-      zoom:5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
+
+
